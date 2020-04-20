0968c7baa6595d05fd7bd9c9368526819291b11c
Peter Bolemant <peter.bolemant@softworx.digital>
2020-04-21 01:11:21 +0200

    [in progress] cms refactoring

--- rx-plugins/keditor/src/components/keditor-component-text.js
+++ rx-plugins/keditor/src/components/keditor-component-text.js
@@ -1,6 +1,9 @@
 import '../styles/keditor-component-text.less';
 
 import KEditor from 'keditor';
+import CKEDITOR from 'ckeditor';
+
+CKEDITOR.disableAutoInline = true;
 
 // Text component
 // ---------------------------------------------------------------------
@@ -30,70 +33,51 @@ KEditor.components['text'] = {
         removeButtons: 'Save,NewPage,Preview,Print,Templates,PasteText,PasteFromWord,Find,Replace,SelectAll,Scayt,Form,HiddenField,ImageButton,Button,Select,Textarea,TextField,Radio,Checkbox,Outdent,Indent,Blockquote,CreateDiv,Language,Table,HorizontalRule,Smiley,SpecialChar,PageBreak,Iframe,Styles,BGColor,Maximize,About,ShowBlocks,BidiLtr,BidiRtl,Flash,Image,Subscript,Superscript,Anchor',
         minimumChangeMilliseconds: 100
     },
-
+    
     init: function (contentArea, container, component, keditor) {
         let self = this;
         let options = keditor.options;
-
+        
         let componentContent = component.children('.keditor-component-content');
         componentContent.prop('contenteditable', true);
-        // componentContent.prop('designMode', 'on');
-
+        
         componentContent.on('input', function (e) {
             if (typeof options.onComponentChanged === 'function') {
                 options.onComponentChanged.call(keditor, e, component);
             }
-
+            
             if (typeof options.onContainerChanged === 'function') {
                 options.onContainerChanged.call(keditor, e, container, contentArea);
             }
-
+            
             if (typeof options.onContentChanged === 'function') {
                 options.onContentChanged.call(keditor, e, contentArea);
             }
         });
-
-        /*
+        
+        let editor = CKEDITOR.inline(componentContent[0], self.options);
         editor.on('instanceReady', function () {
+            $('#cke_' + componentContent.attr('id')).appendTo(keditor.wrapper);
+            
             if (typeof options.onComponentReady === 'function') {
                 options.onComponentReady.call(contentArea, component, editor);
             }
         });
-        */
-
-        if (options.rx.hasPlugin('textarea')) {
-            let $editable = $(keditor.iframe).contents().find(componentContent);
-
-            options.rx.getPlugin('textarea').bindInline($editable);
-            // options.rx.getPlugin('textarea').bindComponent($editable);
-
-            $($editable).on('summernote.init', function() {
-                console.log('Summernote is launched 2');
-            });
-        }
     },
-
+    
     getContent: function (component, keditor) {
-        let options = keditor.options;
-        let componentContent = component.children('.keditor-component-content');
-
-        /*
-        if (options.rx.hasPlugin('textarea')) {
-            return options.rx.getPlugin('textarea').getContent(componentContent);
+        let componentContent = component.find('.keditor-component-content');
+        let id = componentContent.attr('id');
+        let editor = CKEDITOR.instances[id];
+        if (editor) {
+            return editor.getData();
+        } else {
+            return componentContent.html();
         }
-        */
-
-        return componentContent.html();
     },
-
+    
     destroy: function (component, keditor) {
-        let options = keditor.options;
-        let componentContent = component.children('.keditor-component-content');
-
-        /*
-        if (options.rx.hasPlugin('textarea')) {
-            return options.rx.getPlugin('textarea').destroy(componentContent);
-        }
-        */
+        let id = component.find('.keditor-component-content').attr('id');
+        CKEDITOR.instances[id] && CKEDITOR.instances[id].destroy();
     }
 };
